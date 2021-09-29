c05e32cbfa6a83be588ed9c4ea62f4cd3ddd697c
Andres <andresjml@gmail.com>
2021-09-29 15:59:51 -0400

    update trip-2

--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -9,6 +9,7 @@ function TripContainer() {
     const [newItem, setNewItem]=useState({vehicle_id:"", order_id:"", status:false})
     const [orders, setOrders]=useState(null)
     const [updateState, setUpdateState]=useState(false)
+
     
     //READ TRIPS
     useEffect(() => {
@@ -17,6 +18,7 @@ function TripContainer() {
             .then(setTrips)
     }, [updateState]);
 
+
     //READ VEHICLES
     useEffect(() => {
         fetch(BASE_URL +`/vehicles`)
@@ -24,62 +26,14 @@ function TripContainer() {
             .then(setVehicles)
     }, []);
 
+
     //READ ORDERS
     useEffect(() => {
         fetch(BASE_URL +`/orders`)
             .then(r=>r.json())
             .then(setOrders)
     }, []);
-
-    //UPDATE TRIP
-    function onEdit(updatedItem){
-        
-        fetch(BASE_URL + `trips/${updatedItem.id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updatedItem),
-          })
-
-        setUpdateState(!updateState);
-    }
-
-
-
-    //DELETE
-    function onDelete(deletedTrip){
-        
-        fetch(BASE_URL + `trips/${deletedTrip.id}`, {
-            method: "DELETE",
-        })
-        
-        let updatedTrips = trips.filter(trip=>trip.id !== deletedTrip.id)
-        setTrips(updatedTrips)
-    }
-
-    //POPULATE TRIPS
-    function populateTrips(){        
-        return (trips.map(trip => <Trip key={trip.id} trip={trip} onDelete={onDelete} onEdit={onEdit}/>))
-    }
-
-    //POPULATE VEHICLES FOR INPUT FORM
-    function populateVehicles(){
-        return (vehicles.map(vehicle => <option key={vehicle.id} value={vehicle.id} >ID:{vehicle.id}-{vehicle.v_type} / Company: {vehicle.transport_company.name}</option>))
-    }
-
-    //POPULATE ORDERS FOR INPUT FORM
-    function populateOrders(){
-        return (orders.map(order => <option key={order.id} value={order.id} >ID:{order.id}-Client:{order.client.name} / Volume: {order.volume}</option>))
-    }
-
-    //HANDLE INPUT CHANGE
-    function handleInputChange(event) {
-        setNewItem({
-            ...newItem, 
-            [event.target.name]:event.target.value
-        })  
-    } 
+    
 
     //CREATE A NEW TRIP
     function handleSubmit(e){
@@ -96,6 +50,60 @@ function TripContainer() {
         setUpdateState(!updateState);        
         
     } 
+
+
+    //UPDATE TRIP
+    function onEdit(updatedItem){
+        
+        fetch(BASE_URL + `trips/${updatedItem.id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedItem),
+          })
+
+        setUpdateState(!updateState);
+    }
+
+
+    //DELETE
+    function onDelete(deletedTrip){
+        
+        fetch(BASE_URL + `trips/${deletedTrip.id}`, {
+            method: "DELETE",
+        })
+        
+        let updatedTrips = trips.filter(trip=>trip.id !== deletedTrip.id)
+        setTrips(updatedTrips)
+    }
+
+
+    //POPULATE TRIPS
+    function populateTrips(){        
+        return (trips.map(trip => <Trip key={trip.id} trip={trip} onDelete={onDelete} onEdit={onEdit}/>))
+    }
+
+
+    //POPULATE VEHICLES FOR INPUT FORM
+    function populateVehicles(){
+        return (vehicles.map(vehicle => <option key={vehicle.id} value={vehicle.id} >ID:{vehicle.id}-{vehicle.v_type} / Company: {vehicle.transport_company.name}</option>))
+    }
+
+
+    //POPULATE ORDERS FOR INPUT FORM
+    function populateOrders(){
+        return (orders.map(order => <option key={order.id} value={order.id} >ID:{order.id}-Client:{order.client.name} / Volume: {order.volume}</option>))
+    }
+
+
+    //HANDLE INPUT CHANGE
+    function handleInputChange(event) {
+        setNewItem({
+            ...newItem, 
+            [event.target.name]:event.target.value
+        })  
+    }    
 
 
     return (
