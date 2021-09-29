f0b13f8b4c20f53a7e18a47d346c50cb7b437911
Andres <andresjml@gmail.com>
2021-09-29 10:26:53 -0400

    react router dom configured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import OrderContainer from "./components/OrderContainer";
+import TransportCompanyContainer from "./components/TransportCompanyContainer";
+import TripContainer from "./components/TripContainer";
+import VehicleContainer from "./components/VehicleContainer";
+import Home from "./components/Home"
 
 function App() {
   return (
@@ -7,7 +12,24 @@ function App() {
       React App
       <NavBar/>
       <Switch>
-        
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path ="/orders">
+          <OrderContainer/>
+        </Route>
+        <Route path="/transport_companies">
+          <TransportCompanyContainer/>
+        </Route>
+        <Route path="/vehicles">
+          <VehicleContainer/>
+        </Route>
+        <Route path="/trips">
+          <TripContainer/>
+        </Route>
+        <Route path="*">
+          <h1>404 Not Found</h1>
+        </Route>
       </Switch>
       
     </>
