35339527a4456ac366bd714f2dbf18f0bb3df5e7
Andres <andresjml@gmail.com>
2021-09-29 15:23:44 -0400

    create trips

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-function Trip({trip}) {
+function Trip({trip, onDelete}) {
+    
+
+
+
     return (
-        <li>
-            Trip # {trip.id} | Order # {trip.order.id} | Client Name:{trip.order.client.name} | Address:{trip.order.client.address} | Transport Company: {trip.transport_company.name} | Vehicle ID: {trip.vehicle.id}
-        </li>
+        <>
+            {
+                trip&&(
+                
+                <li>           
+                    <br/>
+                    Trip # {trip.id} | Order # {trip.order.id} | Client Name:{trip.order.client.name} | Address:{trip.order.client.address} | Transport Company: {trip.transport_company.name} | Vehicle ID: {trip.vehicle.id}
+                    <br/>
+                    <button>Update</button>        
+                    <button onClick={()=>onDelete(trip)}>Delete</button>            
+                </li>)
+            }
+        
+        </>
     )
 }
 
--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -4,24 +4,113 @@ import { BASE_URL } from "../constraints/index"
 
 function TripContainer() {
     const [trips, setTrips]=useState(null)
+    const [toggle, setToggle]=useState(false)
+    const [vehicles, setVehicles]=useState(null)
+    const [newItem, setNewItem]=useState({vehicle_id:"", order_id:"", status:false})
+    const [orders, setOrders]=useState(null)
+    const [updateState, setUpdateState]=useState(false)
     
     //READ TRIPS
     useEffect(() => {
         fetch(BASE_URL +`/trips`)
             .then(r=>r.json())
             .then(setTrips)
+    }, [updateState]);
+
+    //READ VEHICLES
+    useEffect(() => {
+        fetch(BASE_URL +`/vehicles`)
+            .then(r=>r.json())
+            .then(setVehicles)
     }, []);
+
+    //READ ORDERS
+    useEffect(() => {
+        fetch(BASE_URL +`/orders`)
+            .then(r=>r.json())
+            .then(setOrders)
+    }, []);
+
+
+
+    //DELETE
+    function onDelete(deletedTrip){
+        
+        fetch(BASE_URL + `trips/${deletedTrip.id}`, {
+            method: "DELETE",
+        })
+        
+        let updatedTrips = trips.filter(trip=>trip.id !== deletedTrip.id)
+        setTrips(updatedTrips)
+    }
 
     //POPULATE TRIPS
     function populateTrips(){        
-        return (trips.map(trip => <Trip key={trip.id} trip={trip}/>))
+        return (trips.map(trip => <Trip key={trip.id} trip={trip} onDelete={onDelete}/>))
     }
+
+    //POPULATE VEHICLES FOR INPUT FORM
+    function populateVehicles(){
+        return (vehicles.map(vehicle => <option key={vehicle.id} value={vehicle.id} >ID:{vehicle.id}-{vehicle.v_type} / Company: {vehicle.transport_company.name}</option>))
+    }
+
+    //POPULATE ORDERS FOR INPUT FORM
+    function populateOrders(){
+        return (orders.map(order => <option key={order.id} value={order.id} >ID:{order.id}-Client:{order.client.name} / Volume: {order.volume}</option>))
+    }
+
+    //HANDLE INPUT CHANGE
+    function handleInputChange(event) {
+        setNewItem({
+            ...newItem, 
+            [event.target.name]:event.target.value
+        })  
+    } 
+
+    //CREATE A NEW TRIP
+    function handleSubmit(e){
+        e.preventDefault()        
+        
+        fetch(BASE_URL +`trips`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newItem),
+        })
+        
+        setUpdateState(!updateState);
+        
+        
+    } 
 
 
     return (
-        <ul>
-            {trips&&populateTrips()}
-        </ul>
+        <>
+            <button onClick={()=>setToggle(!toggle)}>New Trip</button>
+            {
+                toggle&&(
+                    <form onSubmit={handleSubmit}>
+                        <select name='vehicle_id' onChange={handleInputChange}>
+                            <option >Select Vehicle</option>
+                            {vehicles&&populateVehicles()}
+                        </select>
+                        <select name='order_id' onChange={handleInputChange}>
+                            <option >Select Order</option>
+                            {orders&&populateOrders()}
+                        </select>
+                        <button type="submit">
+                            Add to Order
+                        </button> 
+                    </form>
+                )
+            }
+
+
+            <ul>
+                {trips&&populateTrips()}
+            </ul>
+        </>
     )
 }
 
